4cf2ca1222769578aa6ff4cb9848ea2a88d6e47c
Rubens Stanley Almeida Brandão <rubensalmeida@sefaz.pi.gov.br>
2022-11-21 11:48:54 -0300

    Dados Listagem de Médicos - GetMapping

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,5 +22,9 @@ public class MedicoController {
     @Transactional
     public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico){
         medicoRepository.save(new Medico(dadosCadastroMedico));
+    }
+    @GetMapping
+    public List<DadosListagemMedicos> listar() {
+        return medicoRepository.findAll().stream().map(DadosListagemMedicos::new).toList();
     }
 }
