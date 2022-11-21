ccbc98006364330fb4ec3c12bf8c80565936332d
Rubens Stanley Almeida Brandão <rubensalmeida@sefaz.pi.gov.br>
2022-11-21 12:40:29 -0300

    Configuração de Paginação Default

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +27,7 @@ public class MedicoController {
         medicoRepository.save(new Medico(dadosCadastroMedico));
     }
     @GetMapping
-    public Page<DadosListagemMedicos> listar(Pageable pageable) {
+    public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable pageable) {
         return medicoRepository.findAll(pageable).map(DadosListagemMedicos::new);
     }
 }
