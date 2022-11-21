9c567eae3a344586bffbd3ed7233db96dd43b391
Rubens Stanley Almeida Brandão <rubensalmeida@sefaz.pi.gov.br>
2022-11-21 12:34:15 -0300

    Paginação

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +26,7 @@ public class MedicoController {
         medicoRepository.save(new Medico(dadosCadastroMedico));
     }
     @GetMapping
-    public List<DadosListagemMedicos> listar() {
-        return medicoRepository.findAll().stream().map(DadosListagemMedicos::new).toList();
+    public Page<DadosListagemMedicos> listar(Pageable pageable) {
+        return medicoRepository.findAll(pageable).map(DadosListagemMedicos::new);
     }
 }
